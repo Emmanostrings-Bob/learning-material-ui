0423e3733060f2118bfe3b4a562b090ad7ca240a
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2024-01-12 10:59:55 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
-import { Typography, AppBar,Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import { Typography, Box, AppBar,Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container,  } from '@mui/material';
 import { PhotoCamera } from '@mui/icons-material';
-import { makeStyles } from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@mui/material/styles'
+ 
 
+const theme = createTheme()
 
-const useStyles = makeStyles((theme)=> ({
-  container: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6)
-  }
-}))
-
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const App = () => {
-  const classes = useStyles()
   return (
-    <>
+    <ThemeProvider theme={theme}>
         <CssBaseline />
         <AppBar position='relative'>
           <Toolbar>
-            <PhotoCamera />
+            <PhotoCamera sx={{marginRight:'20px',}}/>
             <Typography variant='h6'>Photo Album</Typography>
-          </Toolbar>
+          </Toolbar> 
         </AppBar>
         <main>
-          <div className={classes.container}>
+          <Box sx={{ bgcolor: 'background.paper', padding: theme.spacing(8, 0, 6)}}>
             <Container maxWidth='sm'>
-              <Typography variant='h2' align='center' color='textPrimary' hutterButtom>
+              <Typography variant='h2' align='center' color='textPrimary' hutterbuttom='true'>
                 Photo Album
               </Typography>
               <Typography variant='h5' align='center' color='textPrimary' paragraph>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam iste rerum recusandae omnis? Maxime temporibus ex placeat laudantium voluptas eligendi?
               </Typography>
-              <div>
-                <Grid container spacing={2} justify='center'>
+              <div sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center">
+                <Grid container spacing={2} justify='center' sx ={{marginTop:'40px'}}>
                   <Grid item>
                     <Button variant='contained' color='primary'>
                       Explore my photos
@@ -46,9 +44,44 @@ const App = () => {
                 </Grid>
               </div>
             </Container>
-          </div>
+          </Box>
+          <Container sx={{padding:'20px 0'}} maxWidth='md'>
+            <Grid container spacing={4}>
+              {cards.map(() => (
+                  <Grid item key={cards} xs={12} sm={6} md={4}>
+                    <Card sx={{height:'100%', display:'flex', flexDirection:'column'}}>
+                      <CardMedia 
+                      sx={{paddingTop: '56.25%'}}
+                      image='https://source.unsplash.com/random'
+                      title= 'Image Title'
+                      />
+                      <CardContent sx={{flexGrow: 1}}>
+                        <Typography gutterbottom variant='5'>
+                          Heading
+                        </Typography>
+                        <Typography>
+                          This is a media card. You can use this part to decribe the content.
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size='small' color='primary'>View</Button>
+                        <Button size='small' color='primary'>Edit</Button>
+                      </CardActions>
+                    </Card>
+                  </Grid> 
+              ))}
+            </Grid>
+          </Container>
         </main>
-    </>
+        <footer sx={{bgcolor: theme.palette.background.paper, padding:'50px 0'}}>
+          <Typography variant='h6' align='center' gutterBottom>
+            Footer
+          </Typography>
+          <Typography variant='subtitle1' align='center' color= 'textSecondary' gutterBottom>
+            First time using Material Ui, cool dibs!
+          </Typography>
+        </footer>
+    </ThemeProvider>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 
